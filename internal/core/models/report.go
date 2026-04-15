ad4181c5965ed0678c42094af392f28bca4652d7
sathuku <samuelnjiri96@gmail.com>
2026-04-15 19:44:58 +0300

    Update internal/core/models/report.go

--- internal/core/models/report.go
+++ internal/core/models/report.go
@@ -3,9 +3,8 @@ package models
 import "time"
 
 type Report struct {
-	Timestamp time.Time `json:"timestamp"`
-	BusID     string    `json:"bus_id"`
-	Front     int       `json:"front_count"`
-	Rear      int       `json:"rear_count"`
-	SpeedKPH  float64   `json:"speed_kph"`
+	Timestamp      time.Time `json:"timestamp"`
+	BusID          string    `json:"bus_id"`
+	PassengerCount int       `json:"passenger_count"`
+	SpeedKPH       float64   `json:"speed_kph"`
 }
